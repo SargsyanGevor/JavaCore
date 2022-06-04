0ed1b77ac8a3aab402e6be638d41d89c2fe0f937
Gevorg Sargsyan <gyumriparts@gmail.com>
2022-06-04 16:57:15 +0400

    leson5

--- src/homework/PracticeHomework.java
+++ src/homework/PracticeHomework.java
@@ -14,28 +14,21 @@ public class PracticeHomework {
     }
 
     boolean isSameNum(int a, int b) {
-        if (a == b) {
-            return true;
-        }
-        return false;
+        return (a == b);
+
     }
 
     boolean lessThanOrEqualToZero(int number) {
-        if (number <= 0) {
-            return true;
-        }
-        return false;
+        return (number <= 0);
     }
 
     boolean reverseBool(boolean value) {
-        if (value == true) {
-            return false;
+        return  (value == false);
         }
-        return true;
-    }
+
 
     int maxLengt(int[] array1, int[] array2) {
-        if (array1.length > array2.length) {
+        if (array1.length >= array2.length) {
             return array1.length;
         }
         return array2.length;
--- src/homework/PracticeHomeworkDemo.java
+++ src/homework/PracticeHomeworkDemo.java
@@ -4,13 +4,13 @@ public class PracticeHomeworkDemo {
     public static void main(String[] args) {
         PracticeHomework conv = new PracticeHomework();
         int[] array1 = {4, 5, 6, 8, 12};
-        int[] array2 = {6, 12, 95, 58, 98, 12, 5, 12, 98};
+        int[] array2 = {6, 12, 95, 58, 98};
         System.out.println(conv.convert(1485711));
         System.out.println(conv.calcAge(5));
         System.out.println(conv.nextNumber(25));
-        System.out.println(conv.isSameNum(8, 5));
-        System.out.println(conv.lessThanOrEqualToZero(-15));
-        System.out.println(conv.reverseBool(false));
+        System.out.println("a = b " + conv.isSameNum(5, 5));
+        System.out.println("number <= 0 " + conv.lessThanOrEqualToZero(25));
+        System.out.println(conv.reverseBool(true));
         System.out.print(conv.maxLengt(array1,array2));
     }
 }
